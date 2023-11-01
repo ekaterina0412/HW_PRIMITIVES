ca74c76c0f66d608bd43301796196a4295b3907a
Kate <avdoshka1@yandex.ru>
2023-11-01 19:14:39 +0400

    HW_PRIMITIVES

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+        int ticketPrice;
+        int rublsForMile;
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+        ticketPrice = 13_676;
+        rublsForMile = 20;
+
+        int bonusMiles = ticketPrice/rublsForMile;
+
+        System.out.println("Будет начислено бонусов: " + bonusMiles);
+    }
+
+}
